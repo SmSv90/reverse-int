5710fbee0d7a6b8808873c371d48a8ff86c0ae1b
Sviatoslav Smovdyr <48630769+SmSv90@users.noreply.github.com>
2020-10-16 19:12:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  let nStr = n.toString();
+  for (let i = nStr.length-1; i >= 0; i--) {
+    result += nStr[i];
+  }
+  return parseInt(result); 
 }
